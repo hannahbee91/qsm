2437a29c75279adaa5cec215ca6389cab9e2cda2
Hannah Brown <hbrown@getplume.co>
2026-03-08 11:33:46 -0700

    Fixed allowed origins

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
+const allowedOrigins = ["localhost:3000"];
+const appUrl = process.env.AUTH_URL || process.env.NEXTAUTH_URL;
+if (appUrl) {
+  try {
+    allowedOrigins.push(new URL(appUrl).host);
+  } catch {}
+}
+
 const nextConfig: NextConfig = {
   output: "standalone",
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins,
+    },
+  },
 };
 
 export default nextConfig;
